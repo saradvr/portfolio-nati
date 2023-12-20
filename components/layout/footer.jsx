6b7f43a032677fb2709d857fbd5fd4b4ad6c456d
Sara Del Valle <sara.delvallerestrepo@washpost.com>
2023-12-19 21:54:40 -0500

    footer

--- components/layout/footer.jsx
+++ components/layout/footer.jsx
@@ -32,7 +32,7 @@ export default function Footer() {
 		<footer className={css.container}>
 			<Container spacing={['verticalXXLrg', 'bottomLrg']}>
 				<section className={css.sections}>
-					<ul className={css.thanks}>
+					{/* <ul className={css.thanks}>
 						<li><h4>Acknowledgments</h4></li>
 						{
 						content.acknowledgments.map( ({ person, link, note }, index) => {
@@ -57,7 +57,7 @@ export default function Footer() {
 							)
 						})
 						}
-					</ul>
+					</ul> */}
 					<ul className={css.social}>
 						<li><h4>Social</h4></li>
 						<li className={css.socialList}>
@@ -71,7 +71,7 @@ export default function Footer() {
 						</li>
 					</ul>
 				</section>
-				<section className={css.github}>
+				{/* <section className={css.github}>
 					<a href={settings.portfolio.repo_html} rel="noreferrer" target="_blank">
 						<h5>{settings.portfolio.forkthis}</h5>
 						<ul>
@@ -83,7 +83,7 @@ export default function Footer() {
 							</li>
 						</ul>
 					</a>
-				</section>
+				</section> */}
 			</Container>
 			<canvas id="gradient-canvas" className={''} data-transition-in ></canvas>
 		</footer>
