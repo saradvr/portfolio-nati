6ca5e9ff31af1b8802a2ae4f763a0fc8dc72ae3c
Sara Del Valle <sara.delvallerestrepo@washpost.com>
2023-12-19 23:07:55 -0500

    hard skills

--- components/sections/index/technical.jsx
+++ components/sections/index/technical.jsx
@@ -33,26 +33,54 @@ export default function Technical() {
 					preTitle="Hardskills"
 					subTitle="As a creative technologist, I craft intuitive digital experiences using a diverse set of tools and languages."
 				/>
-				<section className={`${about.content} ${about.container}`}>
-					<div className={about.copy}>
+				<section className={`${about.content} ${about.container} ${about.technical}`}>
+					<div className={`${about.copy} ${about.technical}`}>
 						<CopyBlock 
-							title="Polymath foundations"
+							title="User Research"
 							icon={[ 'fat', 'chart-network' ]}
-							copy="With a strong foundation in both design and development, I bring a unique perspective to every project and am constantly learning and evolving to stay up-to-date with the latest industry trends."
+							copy="My knowledge in user research extends to planning and conducting user interviews, along with creating and analyzing surveys or interviews to glean valuable insights into user behaviors and preferences. This foundational understanding guides the entire design process, ensuring that solutions align closely with the actual needs and expectations of the user base."
 							iconClass={about.icon}
-							containerClass={about.container}
+							containerClass={`${about.container} ${about.technical}`}
+						/>
+						<CopyBlock 
+							title="Information Architecture"
+							icon={[ 'fat', 'chart-network' ]}
+							copy="I specialize in developing clear and intuitive structures that facilitate seamless user interactions. This involves creating comprehensive sitemaps and flow diagrams, ensuring that users can easily navigate through the product or platform, enhancing overall usability."
+							iconClass={about.icon}
+							containerClass={`${about.container} ${about.technical}`}
+						/>
+						<CopyBlock 
+							title="UX Writing"
+							icon={[ 'fat', 'chart-network' ]}
+							copy="Writing for UX design is a key aspect of my skill set. I focus on creating clear, concise, and engaging content that guides users through their journey. From microcopy to user interface text, I pay attention to language and tone to enhance the overall user experience and facilitate effective communication."
+							iconClass={about.icon}
+							containerClass={`${about.container} ${about.technical}`}
+						/>
+						<CopyBlock 
+							title="Wireframing"
+							icon={[ 'fat', 'chart-network' ]}
+							copy="My wireframes are iteratively refined based on feedback and usability testing to achieve optimal user interaction. My prototyping capabilities extend to both low and high fidelity, using tools like Figma to build interactive prototypes that facilitate effective design communication."
+							iconClass={about.icon}
+							containerClass={`${about.container} ${about.technical}`}
+						/>
+						<CopyBlock 
+							title="Userflows"
+							icon={[ 'fat', 'chart-network' ]}
+							copy="My approach to user flows involves the creation of comprehensive diagrams that visually represent the steps a user takes to accomplish specific tasks or goals. By mapping out these interactions, I can optimize the overall user experience, making it more efficient and enjoyable."
+							iconClass={about.icon}
+							containerClass={`${about.container} ${about.technical}`}
 						/>
 						<BadgesBlock 
-							title="Software I love to work with" 
-							copy="With over 20 years of experience creating digital assets, I have a deep and intuitive understanding of how the interfaces of the best software products in the world are designed. This is just some of the software I have used over that time."
+							title="Software I work with" 
+							copy="With over 2 years of experience crafting user interfaces, I have engaged with various software applications, gaining insights into the intricate design elements that define the user interface landscape."
 							list={software}
 							block="software" 
 							fullContainer="fullContainer"
 							icon="grid-2-plus"
-							containerClass={about.container}
+							containerClass={`${about.container} ${about.technical}`}
 							headerIcon={about.icon} 
 						/>
-						<BadgesBlock 
+						{/* <BadgesBlock 
 							title="Technologies I love to build with" 
 							copy="I deeply enjoy solving problems through code. From the front end all the way to the back end and the APIs in-between. No project is too challenging for me."
 							list={tech} 
@@ -61,11 +89,23 @@ export default function Technical() {
 							icon="laptop-code"
 							containerClass={about.container}
 							headerIcon={about.icon} 
-						/>							
+						/>							 */}
 					</div>
-					<div className={`${about.image} ${about.technicalSvg}`}>
+					{/* <div className={about.copy}>
+						<BadgesBlock 
+							title="Software I love to work with" 
+							copy="With over 2 years of experience crafting user interfaces, I have engaged with various software applications, gaining insights into the intricate design elements that define the user interface landscape."
+							list={software}
+							block="software" 
+							fullContainer="fullContainer"
+							icon="grid-2-plus"
+							containerClass={about.container}
+							headerIcon={about.icon} 
+						/>
+					</div> */}
+					{/* <div className={`${about.image} ${about.technicalSvg}`}>
 						<Image src="/img/dataism-24.svg" width={477} height={1111} alt="Data Strings 01 by Colorpong: https://ywft.us/2177b695b" />
-					</div>
+					</div> */}
 				</section>	
 			</Container>
 			{/* <SectionGridBg gridSize={4}/> */}
@@ -74,6 +114,17 @@ export default function Technical() {
 }
 
 const software = [
+	{ key: 'illustrator', 	name: 'Illustrator', 		type: 'devicon' },
+	{ key: 'figma', 		name: 'Figma', 				type: 'devicon' },
+	{ key: 'workshop', 		name: 'Optimal Workshop', 				type: 'devicon' },
+	{ key: 'whimsical', 		name: 'Whimsical', 				type: 'devicon' },
+	{ key: 'miro', 		name: 'Miro', 				type: 'devicon' },
+	{ key: 'wave', 		name: 'WAVE', 				type: 'devicon' },
+	{ key: 'color', 		name: 'Color Contrast Checker', 				type: 'devicon' },
+	{ key: 'jira', 		name: 'Jira', 				type: 'devicon' },
+]
+
+const softwareOriginal = [
 	{ key: 'photoshop', 	name: 'Photoshop', 			type: 'devicon' },
 	{ key: 'illustrator', 	name: 'Illustrator', 		type: 'devicon' },
 	{ key: 'figma', 		name: 'Figma', 				type: 'devicon' },
